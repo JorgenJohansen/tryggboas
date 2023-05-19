e4a4fb8699fbb78221ed304a3e9be56b1b3f87f1
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2023-05-19 14:10:09 +0200

    added responsive navbar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,10 +1,15 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
-
+import { Menu } from '@headlessui/react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 const NavBar = () => {
   return (
+    <>
     <div className='text-white bg-slate-800'>
-    <nav className='m-4 p-4 '>
+    <nav className='m-4 p-4 md:block hidden'>
         <Link className='hover:underline text-xl' href='/'>TRYGGBO AS - Boligkompetanse</Link> |
         <Link className='hover:underline text-xl m-2 p-2' href="/om-oss">Om Oss</Link> |
         <Link className='hover:underline text-xl m-2 p-2' href="/vi-tilbyr">Vi tilbyr</Link> |
@@ -13,6 +18,34 @@ const NavBar = () => {
     </nav>
 
     </div>
+    <div className='flex flex-col items-end px-6 py-4  justify-center md:hidden'>
+      <Menu>
+      <Menu.Button>
+        <FontAwesomeIcon icon={faBars}
+        className="ml-2 -mr-1 h-6 w-6 text-2xl "
+        aria-hidden="true"></FontAwesomeIcon>
+      </Menu.Button>
+      <Menu.Items className="flex flex-col m-4 p-4">
+          <Menu.Item >
+            <Link href="/" className='hover:underline text-xl'>TRYGGBO AS - Boligkompetanse</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/om-oss" className='hover:underline text-xl'>Om Oss</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/vi-tilbyr" className='hover:underline text-xl'>Vi tilbyr</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/priser" className='hover:underline text-xl'>Priser</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/kontakt" className='hover:underline text-xl'>Kontakt</Link>
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
+    </div>
+    
+    </>
   )
 }
 
